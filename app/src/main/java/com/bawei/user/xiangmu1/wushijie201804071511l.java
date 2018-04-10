0c03eb5a20e680e43d9cb6562b0491ccf63bc5cb
wsj1997 <1783786457@qq.com>
2018-04-10 11:39:19 +0800

    提交项目

---
+++ app/src/main/java/com/bawei/user/xiangmu1/wushijie201804071511l.java
@@ -0,0 +1,13 @@
+package com.bawei.user.xiangmu1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class wushijie201804071511l extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wushijie201804071511l);
+    }
+}
